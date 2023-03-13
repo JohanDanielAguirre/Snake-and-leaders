695f83507cf99f1346310a1b168e3e8a1c5c6f4e
SebasL07 <sebasgnv0207@gmail.com>
2023-03-13 11:10:41 -0500

    restart project totally on dev

--- src/model/Board.java
+++ src/model/Board.java
@@ -4,5 +4,24 @@ public class Board {
 
     private Tile root;
 
+    public Board(int numberOfTiles){
+        root = new Tile(1);
+        createBoard(numberOfTiles);
+    }
+
+    public void createBoard(int numberTiles){
+        addTile(root,1,numberTiles);
+    }
+
+    private void addTile(Tile pointer, int n, int numTiles){
+
+        if(pointer != null){
+            
+        }
+
+    }
+
+
+
     
 }
--- src/model/Tile.java
+++ src/model/Tile.java
@@ -7,6 +7,44 @@ public class Tile {
     private Tile next;
     private Tile previous;
 
+    private Snake snakeHead;
+    private Snake snakeTail;
+
+    private Ladder start;
+    private Ladder end;
+
+    public Snake getSnakeHead() {
+        return snakeHead;
+    }
+
+    public void setSnakeHead(Snake snakeHead) {
+        this.snakeHead = snakeHead;
+    }
+
+    public Snake getSnakeTail() {
+        return snakeTail;
+    }
+
+    public void setSnakeTail(Snake snakeTail) {
+        this.snakeTail = snakeTail;
+    }
+
+    public Ladder getStart() {
+        return start;
+    }
+
+    public void setStart(Ladder start) {
+        this.start = start;
+    }
+
+    public Ladder getEnd() {
+        return end;
+    }
+
+    public void setEnd(Ladder end) {
+        this.end = end;
+    }
+
     private Player[] players;
 
     public Tile(int numberTile) {
@@ -36,5 +74,13 @@ public class Tile {
 
     public void setPrevious(Tile previous) {
         this.previous = previous;
+    }
+
+    public Player[] getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(Player[] players) {
+        this.players = players;
     }
 }
